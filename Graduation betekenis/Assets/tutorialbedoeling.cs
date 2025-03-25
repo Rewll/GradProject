68a1b8dcaa8050d34ac9516930fdb509eea4f9b6
Meta <Raoulscholtens@gmail.com>
2025-03-25 15:10:56 +0100

    Zo goed als klaar voor de playtest

---
+++ Graduation betekenis/Assets/tutorialbedoeling.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class tutorialbedoeling : MonoBehaviour
+{
+    public GameObject tutorialText1;
+    public GameObject tutorialText2;
+    bool algebeurd = false;
+
+    private void Start()
+    {
+        tutorialText1.SetActive(true);
+        tutorialText2.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetAxis("Mouse X") > 0 || Input.GetAxis("Mouse Y") > 0) && !algebeurd)
+        {
+            algebeurd = true;
+            StartCoroutine(tutorialding());
+        }
+    }
+    IEnumerator tutorialding()
+    {
+        yield return new WaitForSeconds(2f);
+        tutorialText1.SetActive(false);
+        tutorialText2.SetActive(true);
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+        tutorialText2.SetActive(false);
+    }
+}
