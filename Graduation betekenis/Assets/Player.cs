204195240f630e8dc2665998d5be2e82f0990ed0
Meta <Raoulscholtens@gmail.com>
2025-05-08 14:49:56 +0200

    Player manager script toegevoegd

---
+++ Graduation betekenis/Assets/Player.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode CameraKnop;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
