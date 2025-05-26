7c0d590d83d89c4d304dc22431d407b0738546f4
Meta <Raoulscholtens@gmail.com>
2025-05-26 15:15:58 +0200

    player refactor

---
+++ Graduation betekenis/Assets/Scripts/FSM/PlayerFSM/Staten/PlayerSleepState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSleepMode : BaseState
+{
+    private PlayerAgent _playerAgentRef;
+
+    void Awake()
+    {
+        _playerAgentRef = GetComponent<PlayerAgent>();
+    }
+
+    public override void OnEnter()
+    {
+       
+    }
+    
+    public override void OnUpdate()
+    {
+       
+    }
+    
+    public override void OnFixedUpdate()
+    {
+
+    }
+    
+    public override void OnExit()
+    {
+       
+    }
+}
---
+++ Graduation betekenis/Assets/Scripts/FSM/PlayerFSM/Staten/PlayerWalkLookState.cs
@@ -0,0 +1,146 @@
+using System.Transactions;
+using UnityEngine;
+
+public class PlayerMove : BaseState
+{
+    //public PlayerLook PlayerLookRef;
+    [Space] [Header("Movement")] public float moveSpeed;
+    public float groundDrag;
+    [Space] [Header("Ground Check")] public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask whatIsGround;
+    public bool grounded;
+
+    public Transform orientation;
+    [Space] [Header("Gravity")] 
+    public float gravityScale = 1.0f;
+
+    [SerializeField] private bool playerCanFallBelowGround;
+    [SerializeField] private float underNeathGroundTreshold;
+    
+    [Space] 
+    private static float _globalGravity = -9.81f;
+    
+    private float _horizontalInput;
+    private float _verticalInput;
+    private Vector3 _moveDirection;
+    private Rigidbody _RB;
+    private PlayerAgent _playerAgentRef;
+    Vector3 lastGroundPos;
+    
+    
+    void Awake()
+    {
+        _playerAgentRef = GetComponent<PlayerAgent>();
+        _RB = GetComponent<Rigidbody>();
+        _RB.freezeRotation = true;
+        _RB.linearDamping = groundDrag;
+        _playerAgentRef.kameraDisabledMesh.SetActive(false);
+    }
+    public override void OnEnter()
+    {
+        _playerAgentRef.huidigeStaat = playerStates.WalkLookMode;
+        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        if (!_playerAgentRef.playerIsFrozen)
+        {
+            _playerAgentRef.kameraDisabledMesh.SetActive(true);
+        }
+    }
+    
+    public override void OnUpdate()
+    {
+        if (!_playerAgentRef.playerIsFrozen)
+        {
+            // ground check
+            grounded = Physics.CheckSphere(groundCheck.position, groundDistance,whatIsGround);
+            if (grounded)
+            {
+                lastGroundPos = transform.position;
+            }
+
+            _playerAgentRef.playerLookRef.OnUpdate();
+            MyInput();
+            SpeedControl();
+            if (playerCanFallBelowGround)
+            {
+                KeepplayerAfloat();
+            }
+
+            // handle drag
+            /*if (grounded)
+                RB.linearDamping = groundDrag;
+            else
+                RB.linearDamping = 0;*/
+            if (Input.GetKeyDown(_playerAgentRef.CameraKnop))
+            {
+                owner.SwitchState(typeof(Kamera));
+            }
+        }
+    }
+    public override void OnFixedUpdate()
+    {
+        if (!_playerAgentRef.playerIsFrozen)
+        {
+            MovePlayer();
+            Gravity();
+        }
+    }
+    
+    public override void OnExit()
+    {
+        _playerAgentRef.kameraDisabledMesh.SetActive(false);
+    }
+    
+    void MyInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovePlayer()
+    {
+        _moveDirection = orientation.forward * _verticalInput + orientation.right * _horizontalInput;
+        _RB.AddForce(_moveDirection.normalized * (moveSpeed * 10f), ForceMode.Force);
+    }
+    
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(_RB.linearVelocity.x, 0f, _RB.linearVelocity.z);
+
+        // limit velocity if needed
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            _RB.linearVelocity = new Vector3(limitedVel.x, _RB.linearVelocity.y, limitedVel.z);
+        }
+    }
+
+    void Gravity()
+    {
+        if (!grounded)
+        {
+            Vector3 gravity = _globalGravity * gravityScale * Vector3.up;
+            _RB.AddForce(gravity, ForceMode.Acceleration);
+        }
+    }
+
+    public void Teleport(Vector3 destinationPos)
+    {
+        _RB.position = destinationPos;
+    }
+    void KeepplayerAfloat()
+    {
+        if (transform.position.y < underNeathGroundTreshold && !grounded)
+        {
+            //Debug.Log("speler onderwater!");
+            Teleport(lastGroundPos);
+        }
+    }
+    public void SetPlayerRotation(float xRotation, float yRotation)
+    {
+        _playerAgentRef.playerLookRef.xRotation = xRotation;
+        _playerAgentRef.playerLookRef.yRotation = yRotation;
+    }
+}
