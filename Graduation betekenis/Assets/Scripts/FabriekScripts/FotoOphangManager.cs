dfd20665b085a5403453147a3220a6ff69a91b29
Meta <Raoulscholtens@gmail.com>
2025-06-10 16:35:27 +0200

    Deuren toegevoegd aan lege kamers

--- Graduation betekenis/Assets/Scripts/FabriekScripts/FotoOphangManager.cs
+++ Graduation betekenis/Assets/Scripts/FabriekScripts/FotoOphangManager.cs
@@ -10,10 +10,20 @@ using UnityEngine.UI;
 public class FotoOphangManager : MonoBehaviour
 {
     public List<GameObject> fotoOphangImages = new List<GameObject>();
+    public GameObject ophangParent;
     [SerializeField] private int aantalFotos;
     [SerializeField] private int fotoTreshold = 10;
     public UnityEvent onFotoTresholdBereikt;
     public UnityEvent onOphang;
+
+    private void Start()
+    {
+        for (int i = 0; i < ophangParent.transform.childCount; i++)
+        {
+            fotoOphangImages.Add(ophangParent.transform.GetChild(i).gameObject);
+        }
+    }
+
     public void HangFotoOp(Texture pictureTexture)
     {
         fotoOphangImages[aantalFotos].GetComponent<RawImage>().texture = pictureTexture;
