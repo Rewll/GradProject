3923ebb7e221e53414d3f2c0d9323fe0e953e650
Meta <Raoulscholtens@gmail.com>
2025-06-24 13:34:38 +0200

    Vergeten reference te assignen oepsie

--- Graduation betekenis/Assets/viesTriggerScript.cs
+++ Graduation betekenis/Assets/viesTriggerScript.cs
@@ -5,12 +5,7 @@ using UnityEngine.Events;
 public class viesTriggerScript : MonoBehaviour
 {
     public PlayerTrigger playTrigRef;
-
-    private void Start()
-    {
-        playTrigRef.enabled = false;
-    }
-
+    
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
